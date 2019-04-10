6d1ea88c46b91b47a93fd9a0f6c751fdb785a19c
tschaefer <github@blackox.org>
2019-04-10 07:53:21 +0200

    improve firepipe script

--- bin/firepipe.pl
+++ bin/firepipe.pl
@@ -4,16 +4,24 @@ use strict;
 use warnings;
 
 use File::Temp qw(tempfile);
+use Carp qw(croak);
 
 sub run {
-    my ($fh, $file) = tempfile("mutt.XXXXX", TMPDIR => 1, UNLINK => 1);
-    while(<>) {
-        print $fh $_;
-    }
-    close $fh;
+    local $SIG{CHILD} = "IGNORE";
 
-    system("firefox", $file);
-    sleep 5;
+    my $pid = fork;
+    croak("Ooops.\n") if ( !defined $pid );
+
+    if ( $pid == 0 ) {
+        my ( $fh, $file ) = tempfile( "mutt.XXXXX", TMPDIR => 1, UNLINK => 1 );
+        while (<>) {
+            print {$fh} $_;
+        }
+        close $fh or croak("Ooops.\n");
+
+        system "firefox " . $file;
+        sleep 1;
+    }
 
     return 0;
 }
